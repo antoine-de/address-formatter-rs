6a79e429e65e7b6d55d62fa37e34a9bef19c727f
antoine-de <antoine.desbordes@gmail.com>
2019-03-26 09:44:24 +0100

    read templates

--- src/formatter.rs
+++ src/formatter.rs
@@ -5,30 +5,28 @@ use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
 #[derive(Debug)]
+struct Replace(String, String);
+
+#[derive(Debug, Default)]
 struct Template {
     address_template: String,
-    replace: Option<Vec<Vec<String>>>,
-    postformat_replace: Option<Vec<Vec<String>>>,
+    replace: Vec<Replace>,
+    postformat_replace: Vec<Replace>,
     change_country: Option<String>,
     use_country: Option<String>,
 }
 
 #[derive(Debug)]
-struct DefaultTemplate {
-    address_template: Template,
-    fallback_template: Template,
-}
-
-#[derive(Debug)]
 struct Templates {
-    default: DefaultTemplate,
-    templates: HashMap<String, Template>, // todo use a `CountryCode` wrapper as key?
+    default_template: Template,
+    fallback_template: Template,
+    templates_by_country: HashMap<String, Template>, // todo use a `CountryCode` wrapper as key?
 }
 
 pub struct Formatter {
     components: Vec<String>,
     component_aliases: HashMap<String, String>,
-    // templates: Templates,
+    templates: Templates,
     // state_codes: Vec<>,
     // country_to_lang: Vec<>,
     // county_codes: Vec<>,
@@ -73,14 +71,52 @@ impl Formatter {
             }
         }
 
-        // let templates = serde_yaml::from_str(templates_file).unwrap();
+        let raw_templates = yaml_rust::YamlLoader::load_from_str(templates_file)
+            .expect("impossible to read worldwide.yaml file");
+
+        // let raw_templates = &raw_templates[0];
+
+        let default_template = raw_templates[0]["default"]["address_template"]
+            .as_str()
+            .map(|t| Template {
+                address_template: t.to_string(),
+                ..Default::default()
+            })
+            .expect("no default address_template provided");
+        let fallback_template = raw_templates[0]["default"]["fallback_template"]
+            .as_str()
+            .map(|t| Template {
+                address_template: t.to_string(),
+                ..Default::default()
+            })
+            .expect("no default address_template provided");
+
+        let templates_by_country = raw_templates[0]
+        .clone()
+            .into_hash().unwrap()
+            .into_iter()
+            .filter(|(k, _v)| k.as_str().unwrap().to_string().len() == 2) // TODO wrap it in countrycode
+            .map(|(k, v)| {
+                let country_code = k.as_str().unwrap().to_string();
+                let template = Template {
+                    address_template: v["address_template"].as_str().unwrap_or("bob").to_string(),
+                    ..Default::default()
+                };
+                (country_code, template)
+            })
+            .collect();
+        let templates = Templates {
+            default_template,
+            fallback_template,
+            templates_by_country,
+        };
         println!("components: {:?}", &components);
         println!("aliases: {:?}", &component_aliases);
-        // println!("templates: {:?}", &templates);
+        println!("templates: {:?}", &templates);
         Self {
             components,
             component_aliases,
-            // templates
+            templates
         }
     }
 
